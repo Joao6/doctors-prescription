2b0bc4c110b2f37975480a4c6a97ba96ea2cfef9
Joao Pedro <joaopedro.dedepaula@gmail.com>
2017-11-13 22:16:20 -0200

    Update in tasks of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,3 @@
-/**
- * @author  Jozef Butko
- * @url		  www.jozefbutko.com/resume
- * @date    March 2015
- * @license MIT
- *
- * AngularJS Boilerplate: Build, watch and other useful tasks
- *
- * The build process consists of following steps:
- * 1. clean /_build folder
- * 2. compile SASS files, minify and uncss compiled css
- * 3. copy and minimize images
- * 4. minify and copy all HTML files into $templateCache
- * 5. build index.html
- * 6. minify and copy all JS files
- * 7. copy fonts
- * 8. show build folder size
- * 
- */
 var gulp            = require('gulp'),
     browserSync     = require('browser-sync'),
     reload          = browserSync.reload,
@@ -116,100 +97,6 @@ gulp.task('concat', function() {
     .pipe(gulp.dest('./_build/'));
 });
 
-// SASS task, will run when any SCSS files change & BrowserSync
-// will auto-update browsers
-gulp.task('sass', function() {
-  /*return gulp.src('styles/style.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      style: 'expanded'
-    }))
-    .on('error', $.notify.onError({
-      title: 'SASS Failed',
-      message: 'Error(s) occurred during compile!'
-    }))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('styles'))
-    .pipe(reload({
-      stream: true
-    }))
-    .pipe($.notify({
-      message: 'Styles task complete'
-    }));*/
-});
-
-// SASS Build task
-gulp.task('sass:build', function() {
- /*  var s = $.size();
-
-  return gulp.src('styles/style.scss')
-    .pipe($.sass({
-      style: 'compact'
-    }))
-    .pipe($.autoprefixer('last 3 version'))
-    .pipe($.uncss({ */
-     // html: ['./index.html', './views/**/*.html', './components/**/*.html'],
-  /*     ignore: [
-        '.index',
-        '.slick',
-        /\.owl+/,
-        /\.owl-next/,
-        /\.owl-prev/
-      ]
-    }))
-    .pipe($.minifyCss({
-      keepBreaks: true,
-      aggressiveMerging: false,
-      advanced: false
-    }))
-    .pipe($.rename({suffix: '.min'}))
-    .pipe(gulp.dest('_build/css'))
-    .pipe(s)
-    .pipe($.notify({
-      onLast: true,
-      message: function() {
-        return 'Total CSS size ' + s.prettySize;
-      }
-    })); */
-});
-
-// BUGFIX: warning: possible EventEmitter memory leak detected. 11 listeners added.
-require('events').EventEmitter.prototype._maxListeners = 100;
-
-// index.html build
-// script/css concatenation
-gulp.task('usemin', function() {
-  return gulp.src('./index.html')
-    // add templates path
-    .pipe($.htmlReplace({
-      'templates': '<script type="text/javascript" src="js/templates.js"></script>'
-    }))
-    .pipe($.usemin({
-      css: [$.minifyCss(), 'concat'],
-      libs: [$.uglify()],
-      nonangularlibs: [$.uglify()],
-      angularlibs: [$.uglify()],
-      appcomponents: [$.uglify()],
-      mainapp: [$.uglify()]
-    }))
-    .pipe(gulp.dest('./_build/'));
-});
-
-// make templateCache from all HTML files
-gulp.task('templates', function() {
-  return gulp.src([
-      './**/*.html',
-      '!bower_components/**/*.*',
-      '!node_modules/**/*.*',
-      '!_build/**/*.*'
-    ])
-    .pipe($.minifyHtml())
-    .pipe($.angularTemplatecache({
-      module: 'boilerplate'
-    }))
-    .pipe(gulp.dest('_build/js'));
-});
-
 // reload all Browsers
 gulp.task('bs-reload', function() {
   browserSync.reload();
@@ -229,41 +116,25 @@ gulp.task('build:size', function() {
     }));
 });
 
-
-// default task to be run with `gulp` command
-// this default task will run BrowserSync & then use Gulp to watch files.
-// when a file is changed, an event is emitted to BrowserSync with the filepath.
-gulp.task('default', ['browser-sync', 'sass', 'minify-css'], function() {
+gulp.task('default', ['browser-sync'], function() {
   gulp.watch('styles/*.css', function(file) {
     if (file.type === "changed") {
       reload(file.path);
     }
   });
-  gulp.watch(['*.html', 'components/**/*.html', 'views/*.html'], ['bs-reload']);
+  gulp.watch(['*.html', 'components/**/*.html', 'views/**/*.html'], ['bs-reload']);
   gulp.watch(['app/*.js', 'components/**/*.js', 'js/*.js'], ['bs-reload']);
-  gulp.watch('styles/**/*.scss', ['sass', 'minify-css']);
+  gulp.watch('styles/**/*.css', ['bs-reload']);
 });
 
 
 /**
  * build task:
- * 1. clean /_build folder
- * 2. compile SASS files, minify and uncss compiled css
- * 3. copy and minimize images
- * 4. minify and copy all HTML files into $templateCache
- * 5. build index.html
- * 6. minify and copy all JS files
- * 7. copy fonts
- * 8. show build folder size
- * 
  */
 gulp.task('build', function(callback) {
   runSequence(
-    'clean:build',
-    'sass:build',
-    'images',
-    'templates',
-    'usemin',
+    'clean:build',    
+    'images',    
     'fonts',
     'build:size',
     callback);
